54dd5e1eb182f2ad77304621b14313a42c7e63e1
Pedro Machado <pedro.machado@liferay.com>
2024-04-12 08:38:53 -0300

    test: fixing act warning with a mock

--- project-2/src/components/repositories/RepositoriesListItem.test.js
+++ project-2/src/components/repositories/RepositoriesListItem.test.js
@@ -2,6 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
 
+jest.mock("../tree/FileIcon", () => {
+  // content of FileIcon.js
+  return () => {
+    return "File Icon Component";
+  };
+});
+
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
@@ -23,6 +30,4 @@ function renderComponent() {
 
 test("it renders component and shows a link to the repository github page", async () => {
   renderComponent();
-
-  await screen.findByRole("img", { name: "JavaScript" });
 });
