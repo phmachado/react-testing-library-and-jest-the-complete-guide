d0ced8fe1337bb21ae8f990cb846652d95adfab0
Pedro Machado <pfnmachado@gmail.com>
2024-04-14 16:04:06 -0300

    test: `HomeRoute` tests

--- project-2/src/routes/HomeRoute.test.js
+++ project-2/src/routes/HomeRoute.test.js
@@ -6,14 +6,13 @@ import HomeRoute from "./HomeRoute";
 
 const handlers = [
   rest.get("/api/repositories", (req, res, ctx) => {
-    const query = req.url.searchParams.get("q");
-    console.log(query);
+    const language = req.url.searchParams.get("q").split("language:")[1];
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: "full name 1" },
-          { id: 2, full_name: "full name 2" },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
       })
     );
@@ -32,4 +31,36 @@ afterEach(() => {
 
 afterAll(() => {
   server.close();
+});
+
+test("renders two links for each language", async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  // loop over each language
+  const languages = [
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "python",
+    "java",
+  ];
+
+  for (let language of languages) {
+    // for each language, we expect to see two links
+    const links = await screen.findAllByRole("link", {
+      name: new RegExp(`${language}_`),
+    });
+
+    // assert that the links have the correct full_name
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+    expect(links[0]).toHaveAttribute("href", `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute("href", `/repositories/${language}_two`);
+  }
 });
